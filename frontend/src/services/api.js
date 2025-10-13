e2398be383dec24421dc9df49ad71b3ff9417d5f
Pierre NOILHAN <pierrenoilhan58@hotmail.com>
2025-10-13 15:22:39 +0200

    change api URL for VPS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 const API_URL = window.location.hostname === 'localhost' 
   ? 'http://localhost:5001/api' 
-  : '${window.location.protocol}//${window.location.hostname}/api';
+  : `${window.location.protocol}//${window.location.hostname}/api`;
 
 export const loginUser = async (username, password) => {
   const response = await fetch(`${API_URL}/users/login`, {
